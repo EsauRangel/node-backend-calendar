7d7860c97e3f8aa849b9336bd8eedbbb1f5168b4
EsauRangel <esaurangel11@gmail.com>
2023-11-11 00:15:35 -0600

    Se configuro el password encriptado y el login, pendiente jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,28 +1,78 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const Usuario = require('../models/Usuario');
 
+const crearUsuario = async (req, res = express.response) => {
 
-const crearUsuario = (req, res = express.response) => {
-    const { name, email, password } = req.body;
+    const { email, password } = req.body;
+    try {
+        let usuario = await Usuario.findOne({ email });
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: "El usuario ya existe"
+            });
+        }
+        usuario = new Usuario(req.body);
 
-    res.status(201).json({
-        ok: true,
-        msg: "registro",
-        name,
-        email,
-        password
-    });
+        //Encriptar contrasena
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
+        await usuario.save();
+
+        res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name
+        });
+    } catch (error) {
+
+        res.status(500).json({
+            ok: false,
+            msg: "Por favor hable con el administrador",
+            error: error
+        });
+    }
 };
 
 
-const loginUsuario = (req, res = express.response) => {
+const loginUsuario = async (req, res = express.response) => {
     const { email, password } = req.body;
-    
-    res.json({
-        ok: true,
-        msg: "login",
-        email,
-        password
-    })
+
+    try {
+        let usuario = await Usuario.findOne({ email });
+
+        if (!usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: "Usuario y/o contrasena incorrectos"
+            });
+        }
+
+        let validatePassword = bcrypt.compareSync(password, usuario.password);
+        if(!validatePassword){
+            return res.status(400).json({
+                ok: false,
+                msg: "Usuario y/o contrasena incorrectos"
+            });
+        }
+
+        return res.json({
+            ok:true,
+            uid: usuario.id,
+            name: usuario.name
+        });
+
+
+    } catch (error) {
+
+        res.status(500).json({
+            ok: false,
+            msg: "Por favor hable con el administrador",
+            error: error
+        });
+    }
+
 };
 
 
